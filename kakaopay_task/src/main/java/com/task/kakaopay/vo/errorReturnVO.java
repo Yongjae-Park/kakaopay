8037446b2fec187112f532bf33405d6a782ceaa9
yongjae_park <yongjae_park@tmax.co.kr>
2020-11-22 20:25:19 +0900

    Add errorReturnVO.java

--- kakaopay_task/src/main/java/com/task/kakaopay/vo/errorReturnVO.java
+++ kakaopay_task/src/main/java/com/task/kakaopay/vo/errorReturnVO.java
@@ -1,5 +1,18 @@
 package com.task.kakaopay.vo;
 
 public class errorReturnVO {
-
+	private String errorCode;
+	private String errorMessage;
+	public String getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
 }
