9c183d5f25b15ef20fb9482ad895534cd3c85e8c
yongjae_park <yongjae_park@tmax.co.kr>
2020-11-22 22:36:10 +0900

    Add token value random generate logic

--- kakaopay_task/src/test/java/com/task/kakaopay/TokenTest.java
+++ kakaopay_task/src/test/java/com/task/kakaopay/TokenTest.java
@@ -1,11 +1,17 @@
-package com.task.kakaopay.util;
+package com.task.kakaopay;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class TokenUtil {
+import org.junit.jupiter.api.Test;
 
-	public static String generateToken() {
-		
+import com.task.kakaopay.util.TokenUtil;
+
+public class TokenTest {
+
+	@Test
+	public void tokentest() throws NoSuchAlgorithmException {
 		final char[] digits = {
 				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
@@ -14,22 +20,7 @@ public class TokenUtil {
 		        'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
 		        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
 		        'Y', 'Z', '#', '$'
-		};//64진수 -> 3자리 = 2^18의 확률로 겹침 1/262144
-		
-//		int shift = 6;
-//	    char[] buf = new char[64];
-//	    int charPos = 64;
-//	    int radix = 1 << shift;
-//	    long mask = radix - 1;
-//	    long number = v;
-//	    do {
-//	        buf[--charPos] = digits[(int) (number & mask)];//buf 끝에서부터 채워넣음 number가 가장 클때 buf에 들어갈 digit랜덤의 경우가 가장 큼
-//	        number >>>= shift;//2^6만큼 나누기
-//	    } while (number != 0);
-	    
-	    
-	    
-//	    return new String(buf, charPos, (64 - charPos));
+		};
 		int randomIndex = 0;
 		char[] token = new char[3];
 		
@@ -40,6 +31,6 @@ public class TokenUtil {
 			}
 			System.out.println(String.valueOf(token));
 		}
-	    return String.valueOf(token);
 	}
+	
 }
